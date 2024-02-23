4cf37ad44b6c1d7d53da117f7b08065f14d7b842
iyeonghun <twosoull@naver.com>
2024-02-23 17:43:02 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Navbar,Container,Nav,Button,Row,Col} from 'react-bootstrap';
+import data from './data.js'
+import { useState } from 'react';
 function App() {
+
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg"></div>
+      <div className="container">
+        <div className="row">
+          {
+            shoes.map((a,index)=>{
+              return (<상품 shoes={a} index={index}/>);
+            })
+
+          }
+        </div>
+      </div> 
+    </div>
+  );
+}
+
+function 상품(props){
+  console.log(props.index);
+  return( 
+    <div className="col-md-4">
+      <img src={process.env.PUBLIC_URL + "/shoes"+(props.index+1)+".jpeg"} />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
     </div>
   );
 }
